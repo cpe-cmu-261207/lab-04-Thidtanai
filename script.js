93c0bd3e17400af90267667fc740ab7ff1859cf3
thidtanai <thidtanai_k@cmu.ac.th>
2021-07-25 21:17:21 +0700

    first

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-/* Your code here */
+document.querySelector('#Adding_button').onclick = function(){
+    if(document.querySelector('#Adding_list input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#texts').innerHTML += `
+            <div class="texts">
+                <span id="taskname">
+                    ${document.querySelector('#Adding_list input').value}
+                </span>
+                <button class="delete">
+                    <i>-</i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".texts");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#Adding_list input").value = "";
+    }
+}
