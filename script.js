06bedefb28645b54c8e3d477bf5505fdda85ea31
thidtanai <thidtanai_k@cmu.ac.th>
2021-07-25 22:16:29 +0700

    update2

--- script.js
+++ script.js
@@ -8,21 +8,26 @@ document.querySelector('#Adding_button').onclick = function(){
                 <span id="taskname">
                     ${document.querySelector('#Adding_list input').value}
                 </span>
+
+                <button class="done">
+                   <i>done</i>
+                </button>
+                
                 <button class="delete">
-                    <i>-</i>
+                    <i>del</i>
                 </button>
             </div>
         `;
 
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
+        let current_tasks = document.querySelectorAll(".delete");
+        for(let i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
                 this.parentNode.remove();
             }
         }
 
-        var tasks = document.querySelectorAll(".texts");
-        for(var i=0; i<tasks.length; i++){
+        let tasks = document.querySelectorAll(".texts");
+        for(let i=0; i<tasks.length; i++){
             tasks[i].onclick = function(){
                 this.classList.toggle('completed');
             }
@@ -31,3 +36,11 @@ document.querySelector('#Adding_button').onclick = function(){
         document.querySelector("#Adding_list input").value = "";
     }
 }
+
+AddButton.addEventListener('mouseout', () =>{
+    if(text.style.display === 'none'){
+        text.style.display = 'block'
+    }else{
+        text.style.display = 'none'
+    }
+})
